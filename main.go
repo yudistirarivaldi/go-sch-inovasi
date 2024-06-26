4342937fa7616ff72320eb63883e54bbf49e2b16
yudistirarivaldi <yudistirarivaldii1324@gmail.com>
2024-06-26 15:43:01 +0700

    perbaikan

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ import (
 func startScheduler(stopChan <-chan struct{}, appendMessage func(string), wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	ticker := time.NewTicker(1 * time.Second)
+	ticker := time.NewTicker(500 * time.Millisecond)
 	defer ticker.Stop()
 
 	for {
 		select {
 		case <-ticker.C:
-			message := "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32."
+			message := "Hello World!!!!!!!!"
 			appendMessage(message)
 		case <-stopChan:
 			appendMessage("Scheduler stopped")
@@ -37,12 +37,20 @@ func main() {
 	var wg sync.WaitGroup
 	var schedulerRunning bool
 
-	output := widget.NewLabel("")
+	output := widget.NewMultiLineEntry()
+	output.Wrapping = fyne.TextWrapWord
+	output.Disable() // Make it read-only
+
 	scrollContainer := container.NewScroll(output)
 	scrollContainer.SetMinSize(fyne.NewSize(600, 300))
 
+	buffer := ""
+
 	appendMessage := func(message string) {
-		output.SetText(output.Text + message + "\n")
+		buffer += message + "\n"
+		output.SetText(buffer)
+		output.CursorRow = len(buffer)
+		output.Refresh()
 		scrollContainer.ScrollToBottom()
 	}
 
